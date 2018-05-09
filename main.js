4a4d2629d499d8dcc536bcacdb3553eec88e40f9
lyj8330328 <980500807@qq.com>
2018-05-09 18:00:42 +0800

    123

---
+++ main.js
@@ -0,0 +1,10 @@
+ const fibonacci = require('./lib/fibonacci')
+ const year = require('./lib/year')
+
+ const word = require('./lib/word')
+ for(var i=1;i<10;i++)
+   { console.log(fibonacci(i))}
+
+
+  console.log(word("abc"))
+   console.log(year(2000))
